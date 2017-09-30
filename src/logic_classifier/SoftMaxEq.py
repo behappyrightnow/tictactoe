b85afd1dfe5e641ec16edc0376a87ed5f2c877ca
Grant Steinfeld <grantsteinfeld@gmail.com>
2017-09-30 17:37:45 -0400

    simple udacity answer

--- src/logic_classifier/SoftMaxEq.py
+++ src/logic_classifier/SoftMaxEq.py
@@ -4,28 +4,11 @@
 import numpy as np
 
 class SoftMax():
-    def getVstack(self, x):
-        return np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
 
 
     def softmax(self, x):
         # """Compute softmax values for each sets of scores in x."""
-        if (str(type(x)) != "<type 'numpy.ndarray'>"):
-            x = np.array(x)
-            x = self.getVstack(x)
-
-        counter = 0
-        xps = []
-        for row in x:
-            counter = counter + 1
-            if counter == 1:
-                for elem in row:
-                    xps.append(np.exp(elem))
-
-        x[1] = xps
-        sumProbs = np.sum(xps)
-        x[2] = [a / sumProbs for a in xps]
-        return x[2]
+        return np.exp(x) / np.sum(np.exp(x), axis=0)
 
 
 
--- tests/udacity/test_softmax.py
+++ tests/udacity/test_softmax.py
@@ -27,21 +27,25 @@ class TicTacToeTest(unittest.TestCase):
         self.assertItemsEqual(eL, aL, "same values")
 
     def test_check2D(self):
-        scores = [1.0, 2.0, 3.0]
         scores = np.array([[1, 2, 3, 6],
                            [2, 4, 5, 6],
                            [3, 8, 7, 6]])
 
         actual = self.SM.softmax(scores)
         #expected = np.array( [ 0.09003057,  0.24472847,  0.66524096] )
-        expected = np.array([[ 0.09003057,  0.00242826,  0.01587624,  0.33333333],
- [ 0.24472847,  0.01794253,  0.11731043,  0.33333333],
- [ 0.66524096,  0.97962921,  0.86681333,  0.33333333]])
+        expected = np.array([
+            [ 0.09003057,  0.00242826,  0.01587624,  0.33333333],
+            [ 0.24472847,  0.01794253,  0.11731043,  0.33333333],
+            [ 0.66524096,  0.97962921,  0.86681333,  0.33333333]])
+        expected = np.array([
+            [0.09003057317038046, 0.002428258029591337, 0.01587623997646677, 0.33333333333333337],
+            [0.24472847105479767, 0.017942534803329194, 0.11731042782619837, 0.33333333333333337],
+            [0.6652409557748219, 0.9796292071670795, 0.8668133321973348, 0.33333333333333337]])
         print type(expected)
         print type(actual)
         self.assertEquals(type(expected) , type(actual), "actual and expeted same type")
         eL = expected.tolist()
         aL = actual.tolist()
-        print eL, ' ', aL
+        print  aL
 
-        #self.assertItemsEqual(eL, aL, "same values")
+        self.assertItemsEqual(eL, aL, "same values")
